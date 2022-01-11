ea95d9f191974b851a82d1cfd4dc1916aa306bf0
lcw <laicanwen08@hotmail.com>
2022-01-11 13:59:12 +0800

    chore(vue2.0/global): eslint & editor config

--- vue2.0/.eslintrc.js
+++ vue2.0/.eslintrc.js
@@ -1,11 +1,34 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    'browser': true,
+    'node': true,
+    'commonjs': true,
+    'shared-node-browser': false,
+    'worker': false,
+    'amd': true,
+    'mocha': false,
+    'jasmine': false,
+    'jest': true,
+    'phantomjs': false,
+    'qunit': false,
+    'prototypejs': false,
+    'shelljs': false,
+    'meteor': false,
+    'mongo': false,
+    'protractor': false,
+    'applescript': false,
+    'nashorn': false,
+    'serviceworker': false,
+    'atomtest': false,
+    'embertest': false,
+    'webextensions': false,
+    'es6': true,
+    'greasemonkey': false
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/airbnb',
+    '@vue/standard',
   ],
   parserOptions: {
     parser: 'babel-eslint',
@@ -13,6 +36,80 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-restricted-syntax': [
+      'error',
+      {
+        'selector': 'CallExpression[callee.object.name="console"][callee.property.name!=/^(log|warn|error|info|trace)$/]',
+        'message': 'Unexpected property on console object was called'
+      }
+    ],
+    'no-mixed-operators': 'off',
+    'no-tabs': 'off',
+    'quotes': [
+      'error',
+      'single',
+      {
+        'avoidEscape': true,
+        'allowTemplateLiterals': true
+      }
+    ],
+    'semi': [
+      'error',
+      'never',
+      {
+        'beforeStatementContinuationChars': 'never'
+      }
+    ],
+    'comma-dangle': [
+      'error',
+      {
+        'arrays': 'never',
+        'objects': 'never',
+        'imports': 'never',
+        'exports': 'never',
+        'functions': 'never',
+      }
+    ],
+    'no-delete-var': 'error',
+    'prefer-const': [
+      'error',
+      {
+        'ignoreReadBeforeAssign': false
+      }
+    ],
+    'quote-props': [
+      'error',
+      'consistent'
+    ],
+    'vue/name-property-casing': [
+      'error',
+      'PascalCase'
+    ],
+    'vue/attribute-hyphenation': 'off',
+    'vue/html-self-closing': 'off',
+    'vue/component-name-in-template-casing': 'off',
+    'vue/html-closing-bracket-spacing': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/no-parsing-error': 'off',
+    'vue/require-v-for-key': 'error',
+    'vue/no-use-v-if-with-v-for': [
+      'error',
+      {
+        'allowUsingIterationVar': true
+      }
+    ],
+    'vue/max-attributes-per-line': [
+      'warn',
+      {
+        'singleline': 6,
+        'multiline': {
+          'max': 1,
+          'allowFirstLine': false
+        }
+      }
+    ]
   },
   overrides: [
     {
@@ -24,5 +121,5 @@ module.exports = {
         jest: true,
       },
     },
-  ],
+  ]
 };
