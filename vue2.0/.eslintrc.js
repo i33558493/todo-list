8df6048dfbaca5782dfa7325d885ae387d291fbd
lcw <laicanwen08@hotmail.com>
2022-01-13 14:10:44 +0800

    chore(vue2.0/eslint): 增加 vue 文件中 html 部分的校验

--- vue2.0/.eslintrc.js
+++ vue2.0/.eslintrc.js
@@ -27,11 +27,11 @@ module.exports = {
     'greasemonkey': false
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/standard',
+    'plugin:vue/strongly-recommended',
+    '@vue/standard'
   ],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
@@ -67,7 +67,7 @@ module.exports = {
         'objects': 'never',
         'imports': 'never',
         'exports': 'never',
-        'functions': 'never',
+        'functions': 'never'
       }
     ],
     'no-delete-var': 'error',
@@ -115,11 +115,11 @@ module.exports = {
     {
       files: [
         '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
-        jest: true,
-      },
-    },
+        jest: true
+      }
+    }
   ]
-};
+}
